e738ec41b7227a0fa7035d435184a72227fdfc28
Envl <yyaomingm@gmail.com>
2019-05-11 15:56:45 +0200

    clean

--- client/ProductGridItem.js
+++ client/ProductGridItem.js
@@ -1,43 +1,45 @@
-import React from 'react'
+import React from 'react';
 
-const ProductGridItem = ({ item }) => {
-  const { title, price, description, photoUrls } = item
+const ProductGridItem = ({item}) => {
+  const {title, price, description, photoUrls} = item;
   return (
-    <div className='col-xs-6 col-sm-4 col-lg-3'>
+    <div className="product-item">
       <article>
-        <div className='info'>
-          <span className='add-favorite'>
-            <a data-title='Add to favorites' data-title-added='Added to favorites list'>
-              <i className='icon icon-heart' />
+        <div className="info">
+          <span className="add-favorite">
+            <a
+              data-title="Add to favorites"
+              data-title-added="Added to favorites list">
+              <i className="icon icon-heart" />
             </a>
           </span>
           <span>
-            <a className='mfp-open' data-title='Quick wiew'>
-              <i className='icon icon-eye' />
+            <a className="mfp-open" data-title="Quick wiew">
+              <i className="icon icon-eye" />
             </a>
           </span>
         </div>
-        <div className='btn btn-add'>
-          <i className='icon icon-cart' />
+        <div className="btn btn-add">
+          <i className="icon icon-cart" />
         </div>
-        <div className='figure-grid'>
-          <span className='label label-warning'>New</span>
-          <div className='image'>
-            <a className='mfp-open'>
-              <img src={photoUrls && photoUrls[0]} alt='' width='360' />
+        <div className="figure-grid">
+          <span className="label label-warning">New</span>
+          <div className="image">
+            <a className="mfp-open">
+              <img src={photoUrls && photoUrls[0]} alt="" width="360" />
             </a>
           </div>
-          <div className='text'>
-            <h2 className='title h4'>
+          <div className="text">
+            <h2 className="title h4">
               <a>{title}</a>
             </h2>
             <sup>SEK {price}</sup>
-            <span className='description clearfix'>{description}</span>
+            <span className="description clearfix">{description}</span>
           </div>
         </div>
       </article>
     </div>
-  )
-}
+  );
+};
 
-export default ProductGridItem
+export default ProductGridItem;
--- client/Products.js
+++ client/Products.js
@@ -1,27 +1,27 @@
-import { withFirebase } from './firebase'
-import React, { useState, useEffect } from 'react'
-import ProductGridItem from './ProductGridItem'
-import { Link } from 'react-router-dom'
+import {withFirebase} from './firebase';
+import React, {useState, useEffect} from 'react';
+import ProductGridItem from './ProductGridItem';
+import {Link} from 'react-router-dom';
 
 const Products = props => {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
   useEffect(() => {
     props.firebase.db
       .collection('items')
       .get()
       .then(rsl => {
-        let itemArr = []
+        let itemArr = [];
         rsl.forEach(e => {
-          itemArr.push(e.data())
-        })
-        setItems(itemArr)
+          itemArr.push(e.data());
+        });
+        setItems(itemArr);
       })
       .catch(error => {
-        console.log(error)
-      })
-  }, [])
+        console.log(error);
+      });
+  }, []);
 
-  return items.map(item => <ProductGridItem item={item} key={Math.random()} />)
-}
+  return items.map(item => <ProductGridItem item={item} key={Math.random()} />);
+};
 
-export default withFirebase(Products)
+export default withFirebase(Products);
