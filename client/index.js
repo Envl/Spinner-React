b5904c84172f624005def51ab1f893818bad5924
Envl <yyaomingm@gmail.com>
2019-05-14 17:13:38 +0200

    test

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 import {Firebase} from './components/firebase'
+
 import './scss/main.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
