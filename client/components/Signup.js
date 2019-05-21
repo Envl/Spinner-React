eb58d84b1cce797e834ba173fb93fa6e121f228c
Zhongyuan <nanomalloc@gmail.com>
2019-05-21 19:48:18 +0200

    email verify

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react'
-import {withFirebase} from './firebase'
-import {ROUTES} from '../constants'
+import React, { useState, useEffect } from 'react'
+import { withFirebase } from './firebase'
+import { ROUTES } from '../constants'
 
 const SignUp = props => {
   const [email, setEmail] = useState('')
@@ -11,7 +11,7 @@ const SignUp = props => {
   // const emailRef = React.createRef()
   // const passwordRef = React.createRef()
 
-  const {firebase, history} = props
+  const { firebase, history } = props
 
   // useEffect(() => {
   //   const emailInput = emailRef.current
@@ -23,7 +23,11 @@ const SignUp = props => {
   const handleSignUpSubmit = () => {
     firebase.auth
       .createUserWithEmailAndPassword(email, password)
-      .then(authUser =>
+      .then(authUser => {
+        console.log(authUser)
+        authUser.user.sendEmailVerification({
+          url: `https://ehiver.netlify.com`,
+        })
         firebase.user(authUser.user.uid).set(
           {
             email,
@@ -31,15 +35,15 @@ const SignUp = props => {
             id: authUser.user.uid,
             transactions: [],
             items: [],
-            points: 0
+            points: 0,
           },
-          {merge: true}
+          { merge: true },
         )
-      )
+      })
       .then(() => {
         console.log('aaaaaaaaaaaaaaafete')
-        debugger
-        history.push('/')
+        // debugger
+        history.push(ROUTES.verify)
       })
       .catch(error => {
         console.log(`${error.message}`)
@@ -55,14 +59,14 @@ const SignUp = props => {
             console.log('login', res)
             history.push('/')
           })
-          .catch(({code, message}) => {
+          .catch(({ code, message }) => {
             alert(code, message)
           })
         break
       case 'google':
         firebase
           .authWithGoogle()
-          .then(({additionalUserInfo, user}) => {
+          .then(({ additionalUserInfo, user }) => {
             // console.log('12232323', res)
             if (additionalUserInfo.isNewUser) {
               firebase.user(user.uid).set(
@@ -72,9 +76,9 @@ const SignUp = props => {
                   id: user.uid,
                   transactions: [],
                   items: [],
-                  points: 0
+                  points: 0,
                 },
-                {merge: true}
+                { merge: true },
               )
             }
             history.push('/')
@@ -100,18 +104,18 @@ const SignUp = props => {
   }
 
   return (
-    <div className="sign-up-form-group">
-      <form autoComplete="on" onSubmit={handleSubmit}>
-        <label htmlFor="email">Email </label>
+    <div className='sign-up-form-group'>
+      <form autoComplete='on' onSubmit={handleSubmit}>
+        <label htmlFor='email'>Email </label>
         <input
-          id="register-email-input"
-          type="email"
-          name="email"
+          id='register-email-input'
+          type='email'
+          name='email'
           value={email}
           required
-          autoComplete="on"
+          autoComplete='on'
           // ref={emailRef}
-          placeholder="Email: *"
+          placeholder='Email: *'
           onChange={e => setEmail(e.target.value)}
         />
         {/* <label htmlFor='password'>Username</label>
@@ -126,35 +130,31 @@ const SignUp = props => {
           setUsername(event.target.value)
         }}
       /> */}
-        <label htmlFor="password">Password</label>
+        <label htmlFor='password'>Password</label>
         <input
-          id="register-password-input"
-          name="password"
-          type="password"
+          id='register-password-input'
+          name='password'
+          type='password'
           value={password}
           required
-          placeholder="Password"
-          autoComplete="on"
+          placeholder='Password'
+          autoComplete='on'
           // ref={passwordRef}
           onChange={e => setPassword(e.target.value)}
         />
-        <button
-          type="submit"
-          className="btn-signin btn"
-          onClick={() => (btnType = 'email')}>
+        <button type='submit' className='btn-signin btn' onClick={() => (btnType = 'email')}>
           Sign In
         </button>
         <button
-          type="submit"
-          className="btn-signup btn"
+          type='submit'
+          className='btn-signup btn'
           onClick={e => {
             btnType = 'signup'
-          }}>
+          }}
+        >
           Sign up now
         </button>
-        <button
-          className="btn-signin-google btn"
-          onClick={() => (btnType = 'google')}>
+        <button className='btn-signin-google btn' onClick={() => (btnType = 'google')}>
           Or sign in with Google
         </button>
       </form>
--- client/constants.js
+++ client/constants.js
@@ -4,15 +4,14 @@ const ROUTES = {
   signup: '/signup',
   history: '/history',
   homepage: '/homepage',
-  landing: '/landing'
+  landing: '/landing',
+  verify: '/verify',
 }
 
 const AUTH_API = 'https://us-central1-spinner-stuff.cloudfunctions.net/api_user'
-const TRANSAC_API =
-  'https://us-central1-spinner-stuff.cloudfunctions.net/api_transaction'
+const TRANSAC_API = 'https://us-central1-spinner-stuff.cloudfunctions.net/api_transaction'
 
 const ITEM_API = 'https://us-central1-spinner-stuff.cloudfunctions.net/api_item'
-const ALL_ITEM_API =
-  'https://us-central1-spinner-stuff.cloudfunctions.net/api_all_items'
+const ALL_ITEM_API = 'https://us-central1-spinner-stuff.cloudfunctions.net/api_all_items'
 
-export {ROUTES, AUTH_API, ITEM_API, TRANSAC_API, ALL_ITEM_API}
+export { ROUTES, AUTH_API, ITEM_API, TRANSAC_API, ALL_ITEM_API }
